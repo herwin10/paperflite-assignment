e35874b3d7b7ed9b5a9acf97e40af3c5276c8d09
herwin10 <160164222+herwin10@users.noreply.github.com>
2026-02-15 13:36:24 +0530

    Update Singlenumber.java

--- Singlenumber.java
+++ Singlenumber.java
@@ -1,6 +1,9 @@
 public int singleNumber(int[] nums) 
     {
         int res=0;
+        if(nums.length==1){
+            return nums[0];
+        }
         for(int i=0;i<nums.length;i++)
         {
             res=res^nums[i];
